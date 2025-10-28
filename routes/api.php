77290c70a35c567e35aa669e258760ac858257fc
Golam Muktadir <gm.cse.duet@gmail.com>
2025-10-29 00:19:30 +0600

    Service class implemented and add fallback route

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,13 @@ Route::prefix('auth')->group(function(){
     Route::post('register',[AuthController::class,'register']);
     Route::post('login',[AuthController::class,'login']);
 });
+//when no route found | fallback function
+
+Route::fallback(function(){
+    return response()->json([
+        'status' => 'error',
+        'message'=>'API Endpoint not found',
+        'data'=>[],
+        'error'=>[]
+    ]);
+});
